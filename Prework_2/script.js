6a0f508d325d10230bf1642317456d05fa0a0f1c
Touheda Khanom <57848483+Touheda812@users.noreply.github.com>
2023-05-25 22:52:15 -0400

    Add files via upload

--- Prework_2/script.js
+++ Prework_2/script.js
@@ -3,9 +3,9 @@ const output = document.querySelector(".output");
 
 calculateButton.addEventListener("click", function() {
     const billAmount = document.getElementById("billAmount").value;
-    const tipPercentage = 15;
+    const tipPercentage = document.getElementById("tipPercentage").value;
     const tipAmount = (billAmount * (tipPercentage / 100)).toFixed(2);
-    const formattedBillAmount = parseFloat(billAmount).toFixed(2);
+    const totalAmount = (parseFloat(billAmount) + parseFloat(tipAmount)).toFixed(2);
   
-    output.innerHTML = `<h1>You should tip $${tipAmount} on $${formattedBillAmount}</h1>`;
+    output.innerHTML = `<h1>You should tip $${tipAmount} on $${billAmount}.<br>Total amount to be paid: $${totalAmount}</h1>`;
 });
